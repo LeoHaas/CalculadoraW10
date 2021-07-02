1d9054ca8f04b888cf6494bbf9430e76238b5e44
Leo Haas <leonardohaasribeiro@gmail.com>
2021-07-02 11:19:47 -0300

    .

---
+++ funcoes.js
@@ -0,0 +1,67 @@
+function numeros(){
+    var x = event.keyCode 
+
+    console.log("tecla pressionada" + x)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function somar(){ 
+    var PrimeiraSoma = 0;
+    var SegundaSoma = 0;
+
+    PrimeiraSoma = parseFloat(document.getElementById("A").value);
+    SegundaSoma = parseFloat(document.getElementById("B").value);
+
+    alert(PrimeiraSoma + SegundaSoma);
+}
+
+function diminuir(){
+    var PrimeiraDiminuir = 0;
+    var SegundaDiminuir = 0;
+    
+    PrimeiraDiminuir = parseFloat(document.getElementById("A").value);
+    SegundaDiminuir = parseFloat(document.getElementById("B").value);
+    
+    alert(PrimeiraDiminuir - SegundaDiminuir);
+}
+
+function divisao(){
+    var PrimeiraDivisao = 0;
+    var SegundaDivisao = 0;
+    
+    PrimeiraDivisao = parseFloat(document.getElementById("A").value);
+    SegundaDivisao = parseFloat(document.getElementById("B").value);
+    
+    alert(PrimeiraDivisao / SegundaDivisao);
+}
+
+function multiplicacao(){
+    var PrimeiraMultiplicacao = 0
+    var SegundaMultiplicacao = 0
+    
+    PrimeiraMultiplicacao = parseFloat(document.getElementById("A").value)
+    SegundaMultiplicacao = parseFloat(document.getElementById("B").value)
+    
+    alert(PrimeiraMultiplicacao * SegundaMultiplicacao); 
+}
+*/
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Meu primeiro projeto utilizando um pouco mais de JS, juntamente com HTML + CSS. Objetivo é de fazer uma calculadora igual a padrão do Windows 10, mas na web. Imagem base:
+
+
+
+<img src="fotocalculadorabase.png">
